5aeb0a656d186633abd74555fb17d230692a172d
Weronika Kansy <weronika.kansy@gmail.com>
2023-10-06 18:47:03 +0200

    Fix SuperHeader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -23,12 +23,17 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  padding: 8px 32px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
 `;
 
 const MarketingMessage = styled.span`
+  margin-right: auto;
   color: ${COLORS.white};
 `;
 
